9c52a475354a8c7f1c9273a2af91b93be41e3189
[saraibra] <[sarah.ibrahim1855@gmail.com]>
2020-01-21 12:19:42 +0200

    add popular

--- lib/Screens/home_tabs/Popular.dart
+++ lib/Screens/home_tabs/Popular.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Popular extends StatefulWidget {
   @override
   _PopularState createState() => _PopularState();
@@ -7,6 +8,63 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemBuilder: (context, position) {
+        return Card(
+          child: _drawSingleRaw(),
+        );
+      },
+      itemCount: 20,
+    );
   }
+
+    Widget _drawSingleRaw() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            child: Image(
+              image: ExactAssetImage('assets/images/pc.png'),
+              fit: BoxFit.cover,
+            ),
+            width: 124,
+            height: 124,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'The world global warming pla pla ',
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('data'),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.timer),
+                        Text('15 min'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+ 
 }
